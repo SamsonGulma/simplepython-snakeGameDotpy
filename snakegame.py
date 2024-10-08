5d7a0c45b5bcb0a203eee1645d767e78dd9009d4
SamsonGulma <sams0ngultek@gmail.com>
2024-10-08 20:53:14 +0000

    Oct 8, 2024, 11:53 PM

--- snakegame.py
+++ snakegame.py
@@ -5,10 +5,22 @@ yard.setup(width=700, height=700)
 yard.bgcolor('lightgreen')
 yard.title('Snake Game')
 
+all_the_segments = []
+
 segment_starting_position = [(0, 0), (-20, 0), (-40, 0)]
 for single_segment_starts_at in segment_starting_position:
     newly_segment = Turtle('circle')
+    newly_segment.append(all_the_segments)
     newly_segment.color('black')
     newly_segment.goto(single_segment_starts_at)
+
+
+game_over = False
+
+while not game_over:
+    for every_segments in all_the_segments:
+        every_segments.forward(10)
+
+
 
 yard.exitonclick()
