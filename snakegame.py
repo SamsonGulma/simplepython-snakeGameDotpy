51573b84865e06544a77ce0bda531c4623b3768a
SamsonGulma <sams0ngultek@gmail.com>
2024-10-08 23:33:40 +0300

    another commit

--- snakegame.py
+++ snakegame.py
@@ -5,5 +5,9 @@ yard.setup(width=700, height=700)
 yard.bgcolor('lightgreen')
 yard.title('Snake Game')
 
+segment_starting_position = [(0, 20), (-20, 0), (-40, 0)]
+for single_segment in segment_starting_position:
+    newly_segment = Turtle('circle')
+    newly_segment.color('black')
 
 yard.exitonclick()
