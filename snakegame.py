503b3b60042e45e4d94116f6b131eea4985ba1fa
SamsonGulma <sams0ngultek@gmail.com>
2024-10-09 00:12:11 +0300

    this is another commit

--- snakegame.py
+++ snakegame.py
@@ -2,6 +2,7 @@ from turtle import Screen, Turtle
 import time
 
 yard = Screen()
+yard.tracer(0)
 yard.setup(width=700, height=700)
 yard.bgcolor('lightgreen')
 yard.title('Snake Game')
@@ -10,7 +11,7 @@ all_the_segments = []
 
 segment_starting_position = [(0, 0), (-20, 0), (-40, 0)]
 for single_segment_starts_at in segment_starting_position:
-    newly_segment = Turtle('square')
+    newly_segment = Turtle('circle')
     newly_segment.color('black')
     newly_segment.penup()
     newly_segment.goto(single_segment_starts_at)
