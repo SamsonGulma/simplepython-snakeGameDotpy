39e6876f4d739154778b93f68e29020586714271
SamsonGulma <sams0ngultek@gmail.com>
2024-10-08 21:13:24 +0000

    Oct 9, 2024, 12:13 AM

--- snakegame.py
+++ snakegame.py
@@ -23,10 +23,10 @@ game_over = False
 
 while not game_over:
     yard.update()
-    
+    time.sleep(0.1)
     for every_segments in all_the_segments:
         every_segments.forward(10)
-        time.sleep(0.1)
+        
 
 
 yard.exitonclick()
