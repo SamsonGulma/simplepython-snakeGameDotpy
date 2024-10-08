4ba36e541423940c860ce2a6996d2fc1e2b346a7
SamsonGulma <sams0ngultek@gmail.com>
2024-10-08 20:36:38 +0000

    Oct 8, 2024, 11:36 PM

--- snakegame.py
+++ snakegame.py
@@ -5,7 +5,7 @@ yard.setup(width=700, height=700)
 yard.bgcolor('lightgreen')
 yard.title('Snake Game')
 
-segment_starting_position = [(0, 20), (-20, 0), (-40, 0)]
+segment_starting_position = [(0, 0), (-20, 0), (-40, 0)]
 for single_segment_starts_at in segment_starting_position:
     newly_segment = Turtle('circle')
     newly_segment.color('black')
